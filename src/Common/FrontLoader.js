d0e2e93cc390fecf6ef42ad1feea6ecdc753257b
moofawsaw <kylejunderhill@gmail.com>
2022-07-30 14:55:47 -0400

    Update FrontLoader.js

--- src/Common/FrontLoader.js
+++ src/Common/FrontLoader.js
@@ -1,6 +1,7 @@
-const FrontLoader = (props) =>{
+/*const FrontLoader = (props) =>{
     return(
         <div id={props.loading ? "pageLoader":""} className="pageLoader"/>
     )
 }
 export default FrontLoader
+*/
